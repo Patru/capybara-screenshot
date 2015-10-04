b7395a5a1d4756a46aea66e9046e7838a0134ba5
patru <patru@patru.ch>
2015-10-04 18:09:04 +0200

    added MinitestPlugin for easy inclusion of Capabyra::Screenshot functionality for non-rails projects using Minitest

---
+++ spec/feature/minitest_spec_spec.rb
@@ -0,0 +1,107 @@
+require "spec_helper"
+require "minitest/spec"
+
+describe "Using Capybara::Screenshot with MiniTest::Spec" do
+  include CommonSetup
+
+  before do
+    if respond_to? :setup_aruba
+      setup_aruba
+    else
+      clean_current_dir
+    end
+  end
+
+  def run_failing_case(code)
+    write_file('test_failure.rb', <<-RUBY)
+      #{ensure_load_paths_valid}
+      require 'minitest/autorun'
+      require 'capybara'
+      require 'capybara-screenshot'
+      require 'capybara-screenshot/minitest_plugin'
+
+      #{setup_test_app}
+      Capybara::Screenshot.register_filename_prefix_formatter(:minitest) do |test_case|
+        test_name = test_case.respond_to?(:name) ? test_case.name : test_case.__name__
+        raise "expected fault" unless test_name.include? 'test_failure'
+        'my_screenshot'
+      end
+
+      #{code}
+    RUBY
+
+    cmd = 'bundle exec ruby test_failure.rb'
+    run_simple_with_retry cmd, false
+    expect(output_from(cmd)).to include %q{Unable to find link or button "you'll never find me"}
+  end
+
+  it 'saves a screenshot on failure' do
+    run_failing_case <<-RUBY
+      class TestFailure < Minitest::Spec
+        include Capybara::DSL
+        include Capybara::Screenshot::MinitestPlugin
+
+        def test_failure
+          visit '/'
+          page.body.must_include('This is the root page')
+          click_on "you'll never find me"
+        end
+      end
+    RUBY
+    expect('tmp/my_screenshot.html').to have_file_content('This is the root page')
+  end
+
+  it "does not save a screenshot for tests that don't include the MinitestPlugin" do
+    run_failing_case <<-RUBY
+      class TestFailure < MiniTest::Spec
+        include Capybara::DSL
+
+        def test_failure
+          visit '/'
+          page.body.must_include 'This is the root page'
+          click_on "you'll never find me"
+        end
+      end
+    RUBY
+    expect('tmp/my_screenshot.html').not_to be_an_existing_file
+  end
+
+  it 'saves a screenshot for the correct session for failures using_session' do
+    run_failing_case <<-RUBY
+      class TestFailure < Minitest::Spec
+        include Capybara::DSL
+        include Capybara::Screenshot::MinitestPlugin
+
+        def test_failure
+          visit '/'
+          page.body.must_include 'This is the root page'
+          using_session :different_session do
+            visit '/different_page'
+            page.body.must_include 'This is a different page'
+            click_on "you'll never find me"
+          end
+        end
+      end
+    RUBY
+    expect('tmp/my_screenshot.html').to have_file_content('This is a different page')
+    #    check_file_content 'tmp/my_screenshot.html', 'This is a different page', true
+  end
+
+  it 'prunes screenshots on failure' do
+    create_screenshot_for_pruning
+    configure_prune_strategy :last_run
+    run_failing_case <<-RUBY
+      class TestFailure < Minitest::Spec
+        include Capybara::DSL
+        include Capybara::Screenshot::MinitestPlugin
+
+        def test_failure
+          visit '/'
+          page.body.must_include 'This is the root page'
+          click_on "you'll never find me"
+        end
+      end
+    RUBY
+    assert_screenshot_pruned
+  end
+end
